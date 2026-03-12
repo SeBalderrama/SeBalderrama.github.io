bb33b662146380c46ddce4909b6a34bbe61a47f5
SeBalderrama <sebastian.balderr@gmail.com>
2026-03-12 15:25:53 -0400

    preparing for hosting

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,9 @@ import react from "@astrojs/react";
 import { threeMinifier } from "@yushijinhun/three-minifier-rollup";
 
 export default defineConfig({
+  site: "https://sebalderrama.github.io",
+  // Use '/astrosite/' while repo is named "astrosite". When you rename to Sebalderrama.github.io, change to base: '/'
+  base: "/astrosite/",
   integrations: [tailwind(), react()],
   vite: {
     plugins: [
